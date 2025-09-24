b7336c5cbabe307a0d68a5981ddbcbae8264d13a
reda abdo <redaabdoo2005@gmail.com>
2025-09-24 19:15:47 +0300

    fix: clean next.config.ts and ensure proper package.json

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ecommerce.routemisr.com',
-        pathname: '/Route-Academy-products/**',
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        pathname: "/Route-Academy-products/**",
       },
       {
-        protocol: 'https',
-        hostname: 'ecommerce.routemisr.com',
-        pathname: '/Route-Academy-categories/**',
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        pathname: "/Route-Academy-categories/**",
       },
       {
-        protocol: 'https',
-        hostname: 'ecommerce.routemisr.com',
-        pathname: '/Route-Academy-brands/**',
-      }
-      ,
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        pathname: "/Route-Academy-brands/**",
+      },
       {
-        protocol: 'https',
-        hostname: 'ecommerce.routemisr.com',
-        pathname: '/Route-Academy-users/**',
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "ecommerce.routemisr.com",
+        pathname: "/Route-Academy-users/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
